479737999479743cb241d2c6633992628aba872e
TimilsinaBimal <timilsina.bml05@gmail.com>
2020-01-18 15:55:27 +0545

    home page Improvements

--- VisitNepal2020/settings.py
+++ VisitNepal2020/settings.py
@@ -156,6 +156,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
